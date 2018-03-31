1efbecb956c1ba9a15b55d462f345500f5a2c097
K. Eaglin <kceaglin@gmail.com>
2018-03-31 06:05:59 -0400

    routing hotfix

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-// import { Switch, Route }    from 'react-router-dom'
+import { Switch, Route }    from 'react-router-dom'
 import Header               from '../Header/Header'
 import Footer               from '../Footer/Footer'
 import Landing              from '../Landing/Landing'
