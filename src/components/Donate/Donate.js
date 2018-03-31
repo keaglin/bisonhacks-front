e665d269aaa265f0633a766bb780c2e4535d1735
K. Eaglin <kceaglin@gmail.com>
2018-03-31 02:56:02 -0400

    adding new font

--- src/components/Donate/Donate.js
+++ src/components/Donate/Donate.js
@@ -3,7 +3,11 @@ import React from 'react'
 const Donate = () => (
   <div>
     <p>Ready to make a donation?</p>
-    <p></p>
+    <p>Is it damaged?</p>
+    <div>
+      <button></button>
+      <button></button>
+    </div>
     <form>
     </form>
   </div>
