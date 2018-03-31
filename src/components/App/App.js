4330d6603b40b09e2852858e06b3f037186b8db4
Kathy * <kathy.lambert@lotusdesignsagency.com>
2018-03-30 23:42:28 -0400

    Updated Footer component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import './App.css'
+import Header  from "../Header/Header"
+import Footer from "../Footer/Footer"
 
 class App extends Component {
   render() {
