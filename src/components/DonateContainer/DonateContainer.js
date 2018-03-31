c67e2939e41434c6dd0674c66a570468ac32007a
K. Eaglin <kceaglin@gmail.com>
2018-03-31 12:36:18 -0400

    updated color and removed shadow

--- src/components/DonateContainer/DonateContainer.js
+++ src/components/DonateContainer/DonateContainer.js
@@ -7,14 +7,32 @@ class DonateContainer extends Component {
   state = {
     centers: [],
     searchTerm: '',
-    donate: true
+    location: '',
+    damage: true
+  }
+
+  handleSearchTerm = (e) => {
+    if (e.target.value) this.setState({searchTerm: e.target.value})
+  }
+  handleLocation = (e) => {
+    if (e.target.value) this.setState({location: e.target.value})
+  }
+  handleDamage = (e) => {
+    if (e.target.value) this.setState({damage: e.target.value})
+  }
+  
+  getCenters = () => {
+    // if it's damaged, get a list of recycling centers
+    // if it's not, get a list of donation sites
+    axios.get(`${SEARCH_URL}+${this.state.searchTerm}`, )
+    .then(res => this.setState({centers: res.data}))
   }
 
   handleSubmit = (e) => {
     e.preventDefault()
-    axios.get(`${SEARCH_URL}+${this.state.searchTerm}`)
-    .then(res => this.setState({centers: res.data}))
+    this.getCenters()
   }
+
     
   render () {
     return (
@@ -22,43 +40,25 @@ class DonateContainer extends Component {
         <h3>Ready to make a difference?</h3>
         <p>Thank you! Let us get a little information from you so we can help you maximize your impact in the ways that are important to you!</p>
         <div class="row">
-          <form class="col s12">
+          <form class="col s12" onSubmit={this.handleSubmit}>
             <div class="row">
-              <div class="input-field col s6">
+              <div class="input-field col s4 offset-s4">
                 <input placeholder="Placeholder" id="first_name" type="text" class="validate" />
                 <label for="first_name">First Name</label>
               </div>
-              <div class="input-field col s6">
+              <div class="input-field col s4 offset-s4">
                 <input id="last_name" type="text" class="validate" />
                 <label for="last_name">Last Name</label>
               </div>
             </div>
             <div class="row">
-              <div class="input-field col s12">
+              <div class="input-field col s4 offset-s4">
                 <input disabled value="I am not editable" id="disabled" type="text" class="validate" />
                 <label for="disabled">Disabled</label>
               </div>
             </div>
             <div class="row">
-              <div class="input-field col s12">
-                <input id="password" type="password" class="validate" />
-                <label for="password">Password</label>
-              </div>
-            </div>
-            <div class="row">
-              <div class="input-field col s12">
-                <input id="email" type="email" class="validate" />
-                <label for="email">Email</label>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col s12">
-                This is an inline input field:
-                <div class="input-field inline">
-                  <input id="email_inline" type="email" class="validate" />
-                  <label for="email_inline">Email</label>
-                  <span class="helper-text" data-error="wrong" data-success="right">Helper text</span>
-                </div>
+              <div className="input-field col s4 offset-s4">
               </div>
             </div>
           </form>
