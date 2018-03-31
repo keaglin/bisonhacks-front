625115c87fb1951d661065d1dc112e63d7b19a28
K. Eaglin <kceaglin@gmail.com>
2018-03-31 00:03:47 -0400

    adding skeleton for App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react'
+import { Switch, Route } from 'react-router-dom'
 import './App.css'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main>
+          <h1>Hello world</h1>
+          <Switch>
+          </Switch>
+        </main>
       </div>
     )
   }
