badd9d80fc08332963a18b3d01b328e358204819
K. Eaglin <kceaglin@gmail.com>
2018-03-31 13:52:58 -0400

    faked results

--- src/components/DonateContainer/DonateContainer.js
+++ src/components/DonateContainer/DonateContainer.js
@@ -22,9 +22,24 @@ class DonateContainer extends Component {
   }
   
   getCenters = () => {
+    console.log('search term is', this.state.searchTerm)
+    console.log('full search link', SEARCH_URL + this.state.searchTerm)
     // if it's damaged, get a list of recycling centers
     // if it's not, get a list of donation sites
-    axios.get(`${SEARCH_URL}+${this.state.searchTerm}`, )
+    // axios.post(`${SEARCH_URL}` + `${this.state.searchTerm}`, )
+    // .then(res => this.setState({centers: res.data}))
+
+    axios.post({
+      url: `${SEARCH_URL}` + `${this.state.searchTerm}`,
+        type: 'GET',
+        dataType: 'jsonp',
+        cors: true ,
+        contentType:'application/json',
+        secure: true,
+                  headers: {
+                      'Access-Control-Allow-Origin': '*',
+                  }}
+    )
     .then(res => this.setState({centers: res.data}))
   }
 
@@ -35,31 +50,48 @@ class DonateContainer extends Component {
 
     
   render () {
+    let centers = this.state.centers
+    console.log(centers)
     return (
       <div>
-        <h3>Ready to make a difference?</h3>
-        <p>Thank you! Let us get a little information from you so we can help you maximize your impact in the ways that are important to you!</p>
-        <div class="row">
-          <form class="col s12" onSubmit={this.handleSubmit}>
-            <div class="row">
-              <div class="input-field col s4 offset-s4">
-                <input placeholder="Placeholder" id="first_name" type="text" class="validate" />
-                <label for="first_name">First Name</label>
-              </div>
-              <div class="input-field col s4 offset-s4">
-                <input id="last_name" type="text" class="validate" />
-                <label for="last_name">Last Name</label>
-              </div>
-            </div>
-            <div class="row">
-              <div class="input-field col s4 offset-s4">
-                <input disabled value="I am not editable" id="disabled" type="text" class="validate" />
-                <label for="disabled">Disabled</label>
-              </div>
-            </div>
-            <div class="row">
+        <div className="row">
+          <div className="col s12">
+            <h3>Ready to make a difference?</h3>
+          </div>
+          <div className="col s4 offset-s4 center">
+            <p>Thank you! Let us get a little information from you so we can help you maximize your impact in the ways that are important to you!</p>
+          </div>
+        </div>
+        <div className="row">
+          <form className="col s12" onSubmit={this.handleSubmit}>
+            <div className="row">
               <div className="input-field col s4 offset-s4">
+                <input placeholder="Who do you want to help?" id="search_term" type="text" onChange={this.handleSearchTerm}/>
+                <label htmlFor="search_term">Search</label>
               </div>
+              {/* <div className="input-field col s4 offset-s4">
+                <input placeholder="What state are you in? (2 letter abbreviation)" id="location" type="text" />
+                <label htmlFor="location">Location</label>
+              </div> */}
+            </div>
+            {/* <div className="row">
+              <div className="col s4 offset-s4">
+                <p>
+                  <label htmlFor="damage">
+                    <input id="damage" type="radio" />
+                    <span>Yes</span>
+                  </label>
+                </p>
+                <p>
+                  <label htmlFor="damage">
+                    <input id="damage" type="radio" checked />
+                    <span>No</span>
+                  </label>
+                </p>
+              </div>
+            </div> */}
+            <div className="col s4 offset-s4">
+              <input type="submit" className="btn" />
             </div>
           </form>
         </div>
--- src/components/DonateList/DonateList.js
+++ src/components/DonateList/DonateList.js
@@ -1,19 +1,31 @@
 import React from 'react'
+import results from '../../helpers/fakedresults.json'
 import './DonateList.css'
 
-const DonateList = (props) => (
-  <div className="row">
-    <div className="col s12">
-        <h3> 
-        {props.location.state.donate ? 'Donation ' : 'Recycling '} 
-        Centers </h3>
+const DonateList = (props) => {
+  console.log('results is', results)
+  return (
+    <div className="row">
+      <div className="col s12">
+          <h3> 
+          {/* {props.location.state.donate ? 'Donation ' : 'Recycling '}   */}
+          Donation Centers </h3>
+      </div>
+      <div className="col s6">
+      <h5 className=''> List </h5>
+        <ul>
+          {
+            results.map((result, index) => {
+              console.log(result)
+              return <li key={index}>{result.name}</li>
+            })
+          }
+        </ul>
+      </div>
+      <div className="col s6">
+      <h5 className=''> Map View </h5>
+      </div>
     </div>
-    <div className="col s6">
-    <h5 className=''> List </h5>
-    </div>
-    <div className="col s6">
-    <h5 className=''> Map View </h5>
-    </div>
-  </div>
-)
+  )
+}
 export default DonateList
