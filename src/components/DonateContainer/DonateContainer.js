86e8eff699da35193426df6aeb3e0a538bd8df8d
K. Eaglin <kceaglin@gmail.com>
2018-03-31 10:34:40 -0400

    initializing heroku repo

--- src/components/DonateContainer/DonateContainer.js
+++ src/components/DonateContainer/DonateContainer.js
@@ -19,26 +19,50 @@ class DonateContainer extends Component {
   render () {
     return (
       <div>
-        <h1>Ready to make a donation?</h1>
-        <p>Thank you! Let us get a little information from you so we can direct you to the proper organizations for your contribution</p>
-        <p>Is it damaged?</p>
-        <div>
-          <Link to={{
-            pathname: '/results',
-            state: { donate: false }
-          }}>
-          <button>Yes</button>
-          </Link>
-          <Link to={{
-            pathname: '/results',
-            state: { donate: true }
-          }}>
-          <button>No</button>
-          </Link>
+        <h3>Ready to make a difference?</h3>
+        <p>Thank you! Let us get a little information from you so we can help you maximize your impact in the ways that are important to you!</p>
+        <div class="row">
+          <form class="col s12">
+            <div class="row">
+              <div class="input-field col s6">
+                <input placeholder="Placeholder" id="first_name" type="text" class="validate" />
+                <label for="first_name">First Name</label>
+              </div>
+              <div class="input-field col s6">
+                <input id="last_name" type="text" class="validate" />
+                <label for="last_name">Last Name</label>
+              </div>
+            </div>
+            <div class="row">
+              <div class="input-field col s12">
+                <input disabled value="I am not editable" id="disabled" type="text" class="validate" />
+                <label for="disabled">Disabled</label>
+              </div>
+            </div>
+            <div class="row">
+              <div class="input-field col s12">
+                <input id="password" type="password" class="validate" />
+                <label for="password">Password</label>
+              </div>
+            </div>
+            <div class="row">
+              <div class="input-field col s12">
+                <input id="email" type="email" class="validate" />
+                <label for="email">Email</label>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col s12">
+                This is an inline input field:
+                <div class="input-field inline">
+                  <input id="email_inline" type="email" class="validate" />
+                  <label for="email_inline">Email</label>
+                  <span class="helper-text" data-error="wrong" data-success="right">Helper text</span>
+                </div>
+              </div>
+            </div>
+          </form>
         </div>
-        <form onSubmit={this.handleSubmit}>
-          
-        </form>
       </div>
     )
   }
