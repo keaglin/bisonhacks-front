96e59b0189c4dbd25a7ff66d9163ef75c3e1c351
Marquet Reid <you@example.com>
2018-03-31 00:26:37 -0400

    changes to header and footer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,12 +7,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      <Header />
         <header className="App-header">
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Footer />
       </div>
     )
   }
