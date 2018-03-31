3aac0d2e632ae96a99d8cdcb9ec24b56e12ef2b8
Kathy * <kathy.lambert@lotusdesignsagency.com>
2018-03-31 05:40:57 -0400

    Added stats.

--- planning/pitch.md
+++ planning/pitch.md
@@ -3,6 +3,13 @@
 When It Matters
 Empower yourself to donate to those most in need
 
+Give Smarter
+
+
+### Stats
+Total Income in M (2011) OTAL INCOME IN MILLIONS (2011)	PUBLIC SUPPORT IN MILLIONS (2011)	PERCENTAGE OF TOTAL EXPENSES SPENT ON PROGRAMS†	SALARY & BENEFITS OF HIGHEST-PAID OFFICIAL†
+Goodwill Industries International	goodwill.org	Social services	4,437.0	778.0	89.0‡‡	Highest paid offical: $508,571
+
 ## Impact
 
 
--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -8,10 +8,13 @@ import './Landing.css'
 const Landing = () => (
     <React.Fragment>
       <div className='landing white-text'> 
-        <header className='heading'>
+        <header className='header'>
           <h3 className='logo'>When It Matters</h3>
           <h5 className='slogan'>Empower yourself to donate to those most in need</h5>
         </header>
+        <main>
+          
+        </main>
       </div>
     </React.Fragment>
 )
