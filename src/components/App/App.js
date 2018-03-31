fc410ef310cc9c653ebe18908b0eeef88e7ef167
Marquet Reid <you@example.com>
2018-03-31 02:07:03 -0400

    made landing component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,13 @@
 import React, { Component } from 'react'
 import './App.css'
-import Header  from "../Header/Header"
-import Footer from "../Footer/Footer"
+import Landing  from "../Landing/Landing"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Header />
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Footer />
+        <Landing />
+        {/* <Header /> */}
       </div>
     )
   }
---
+++ src/components/Landing/Landing.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from '../Header/Header'
+import Footer from '../Footer/Footer'
+
+import './Landing.css'
+
+const Landing = () => (
+    <React.Fragment>
+        <Header />
+        <header className="App-header">
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Footer />
+    </React.Fragment>
+)
+
+export default Landing
