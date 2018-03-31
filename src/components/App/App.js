589f1a98395d0f650a2513f52e97a2a6f8310c47
K. Eaglin <kceaglin@gmail.com>
2018-03-31 01:51:15 -0400

    working on integrating mapbox

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { Switch, Route }    from 'react-router-dom'
 import Header               from '../Header/Header'
 import Footer               from '../Footer/Footer'
+import Landing              from '../Landing/Landing'
+import Map                  from '../Map/Map'
 import './App.css'
 
 
@@ -11,8 +13,9 @@ class App extends Component {
       <div className="App">
         <Header />
         <main>
-          Welcome to Logo
           <Switch>
+            <Route exact path='/' render={() => <Landing />} />
+            <Route path='/test' render={() => <Map />} />
           </Switch>  
         </main>
         <Footer />
---
+++ src/components/Landing/Landing.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Landing = () => {
+  return (
+    <div>
+      <p>Welcome to Logo</p>
+    </div>
+  )
+}
+
+export default Landing
