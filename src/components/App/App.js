28e21e8cf6cee7f103ba49ee3d87e6eb7d51fa6e
K. Eaglin <kceaglin@gmail.com>
2018-10-31 14:32:58 -0400

    moving some stuff around; lifting up state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,11 +6,43 @@ import Landing              from '../Landing/Landing'
 import DonateContainer      from '../DonateContainer/DonateContainer'
 import DonateList           from '../DonateList/DonateList'
 import About                from '../About/About'
+import { SEARCH_URL }  from '../../helpers/constants'
+import axios from 'axios'
 // import Map                  from '../Map/Map'
 import './App.css'
 import Login from '../Login/Login'
 
 class App extends Component {
+  state = {
+    centers: [],
+    // location: '',
+    // damage: true
+    // not sure what we were doing here lol
+  }
+
+  getCenters = search => {
+    // do the search and put what we need in state for displaying
+    // console.log('search term is', this.state.searchTerm)
+    // console.log('full search link', SEARCH_URL + this.state.searchTerm)
+    // if it's damaged, get a list of recycling centers
+    // if it's not, get a list of donation sites
+    // axios.post(`${SEARCH_URL}` + `${this.state.searchTerm}`, )
+    // .then(res => this.setState({centers: res.data}))
+
+    axios.post(`${SEARCH_URL}` + `${search}`
+        // type: 'GET',
+        // dataType: 'jsonp',
+        // cors: true ,
+        // contentType:'application/json',
+        // secure: true,
+        //           headers: {
+        //               'Access-Control-Allow-Origin': '*',
+        //           }}
+    )
+    .then(res => this.setState({centers: res.data}))
+    .catch(err => console.log('An error occurred', err))
+  }
+
   render() {
     return (
       <div className="App">
@@ -18,10 +50,10 @@ class App extends Component {
         <main>
           <Switch>
             <Route exact path='/' render={() => <Landing />} />
-            <Route path='/home' render={() => <Landing />} />
+            {/* <Route path='/home' render={() => <Landing />} /> */}
             <Route path='/about' render={() => <About />} />
-            <Route path='/donate' render={() => <DonateContainer />} />
-            <Route path='/results' render={(props) => (<DonateList {...props} />)} />
+            <Route path='/donate' render={() => <DonateContainer getCenters={this.getCenters} />} />
+            <Route path='/results' render={props => (<DonateList {...props} />)} />
             <Route path='/login' render={() => <Login />} />
             {/* <Route path='/test' render={() => <Map />} /> */}
           </Switch> 
--- src/components/DonateContainer/DonateContainer.js
+++ src/components/DonateContainer/DonateContainer.js
@@ -1,102 +1,63 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import { SEARCH_URL }  from '../../helpers/constants'
-import axios from 'axios'
+import React from 'react'
 
-class DonateContainer extends Component {
-  state = {
-    centers: [],
-    searchTerm: '',
-    location: '',
-    damage: true
-  }
 
-  handleSearchTerm = (e) => {
-    if (e.target.value) this.setState({searchTerm: e.target.value})
-  }
-  handleLocation = (e) => {
-    if (e.target.value) this.setState({location: e.target.value})
-  }
-  handleDamage = (e) => {
-    if (e.target.value) this.setState({damage: e.target.value})
-  }
-  
-  getCenters = () => {
-    console.log('search term is', this.state.searchTerm)
-    console.log('full search link', SEARCH_URL + this.state.searchTerm)
-    // if it's damaged, get a list of recycling centers
-    // if it's not, get a list of donation sites
-    // axios.post(`${SEARCH_URL}` + `${this.state.searchTerm}`, )
-    // .then(res => this.setState({centers: res.data}))
+const DonateContainer = () => {
 
-    axios.post({
-      url: `${SEARCH_URL}` + `${this.state.searchTerm}`,
-        type: 'GET',
-        dataType: 'jsonp',
-        cors: true ,
-        contentType:'application/json',
-        secure: true,
-                  headers: {
-                      'Access-Control-Allow-Origin': '*',
-                  }}
-    )
-    .then(res => this.setState({centers: res.data}))
-  }
-
-  handleSubmit = (e) => {
+  createSearch = (e) => {
+    // this is where we gonna get what we need from the form to compose the user-input part of the search link
     e.preventDefault()
-    this.getCenters()
-  }
+    const search
 
-    
-  render () {
-    let centers = this.state.centers
-    console.log(centers)
-    return (
-      <div>
-        <div className="row">
-          <div className="col s12">
-            <h3>Ready to make a difference?</h3>
-          </div>
-          <div className="col s4 offset-s4 center">
-            <p>Thank you! Let us get a little information from you so we can help you maximize your impact in the ways that are important to you!</p>
-          </div>
+    this.props.getCenters(search)
+    e.currentTarget.reset()
+  }
+  let centers = this.state.centers
+  console.log(centers)
+  return (
+    <div>
+      <div className="row">
+        <div className="col s12">
+          <h3>Ready to make a difference?</h3>
         </div>
-        <div className="row">
-          <form className="col s12" onSubmit={this.handleSubmit}>
-            <div className="row">
-              <div className="input-field col s4 offset-s4">
-                <input placeholder="Who do you want to help?" id="search_term" type="text" onChange={this.handleSearchTerm}/>
-                <label htmlFor="search_term">Search</label>
-              </div>
-              {/* <div className="input-field col s4 offset-s4">
-                <input placeholder="What state are you in? (2 letter abbreviation)" id="location" type="text" />
-                <label htmlFor="location">Location</label>
-              </div> */}
-            </div>
-            {/* <div className="row">
-              <div className="col s4 offset-s4">
-                <p>
-                  <label htmlFor="damage">
-                    <input id="damage" type="radio" />
-                    <span>Yes</span>
-                  </label>
-                </p>
-                <p>
-                  <label htmlFor="damage">
-                    <input id="damage" type="radio" checked />
-                    <span>No</span>
-                  </label>
-                </p>
-              </div>
-            </div> */}
-            <div className="col s4 offset-s4">
-              <input type="submit" className="btn" />
-            </div>
-          </form>
+        <div className="col s4 offset-s4 center">
+          <p>Thank you! Let us get a little information from you so we can help you maximize your impact in the ways that are important to you!</p>
         </div>
       </div>
-    )
-  }
+      <div className="row">
+        <form className="col s12" onSubmit={this.createSearch}>
+          <div className="row">
+            <div className="input-field col s4 offset-s4">
+              <input placeholder="Who do you want to help?" id="search_term" type="text"/>
+              <label htmlFor="search_term">Search</label>
+            </div>
+            {/* <div className="input-field col s4 offset-s4">
+              <input placeholder="What state are you in? (2 letter abbreviation)" id="location" type="text" />
+              <label htmlFor="location">Location</label>
+            </div> */}
+          </div>
+          {/* <div className="row">
+            <div className="col s4 offset-s4">
+              <p>
+                <label htmlFor="damage">
+                  <input id="damage" type="radio" />
+                  <span>Yes</span>
+                </label>
+              </p>
+              <p>
+                <label htmlFor="damage">
+                  <input id="damage" type="radio" checked />
+                  <span>No</span>
+                </label>
+              </p>
+            </div>
+          </div> */}
+          <div className="col s4 offset-s4">
+            <input type="submit" className="btn" />
+          </div>
+        </form>
+      </div>
+    </div>
+  )
 }
+
 export default DonateContainer
